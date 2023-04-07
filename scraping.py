078e5700fe50227a0823741a5cda2423e5fdf3b9
Nikhil Kumar Singh <nikhilkumarsingh00098@gmail.com>
2023-04-07 16:25:11 +0530

    Using pandas to save the data

--- scraping.py
+++ scraping.py
@@ -1,45 +1,53 @@
 from bs4 import BeautifulSoup
 import requests, openpyxl
+import pandas as pd
 
-excel1 = openpyxl.Workbook()
-sheet1 = excel1.active
-sheet1.title = 'Images Links'
-sheet1.append(['Images Link'])
 
-excel2 = openpyxl.Workbook()
-sheet2 = excel2.active
-sheet2.title = 'Anchors Links'
-sheet2.append(['Anchors Link'])
+
+#excel1 = openpyxl.Workbook()
+#sheet1 = excel1.active
+#sheet1.title = 'Images Links'
+#sheet1.append(['Images Link'])
+
+#excel2 = openpyxl.Workbook()
+#sheet2 = excel2.active
+#sheet2.title = 'Anchors Links'
+#sheet2.append(['Anchors Link'])
 
 url = "https://plain-train-pike.cyclic.app/"
 
+# Get the HTML
 req = requests.get(url)
 hcont = req.content
 
+# Parse the HTML
 be_so = BeautifulSoup(hcont, 'html.parser')
 
 # All Images links
 
 im_li = be_so.find_all('img')
-
+im_list = []
 
 for ili in im_li:
     if ili.get('src') is not None:
         ili_text = "https://plain-train-pike.cyclic.app/"+ili.get('src')
         print(ili_text)
-        sheet1.append([ili_text])
+        im_list.append(ili_text)
+        #sheet1.append([ili_text])
 
 
 # All a tags links
 
 atag = be_so.find_all('a')
+atag_list = []
 
 for li in atag:
-    if(li.get('href') != '#'):
+    if(li.get('href') != '/'):
         if li.get('href') is not None:
             li_text = "https://plain-train-pike.cyclic.app"+li.get('href')
             print(li_text)
-            sheet2.append([li_text])
+            atag_list.append(li_text)
+            #sheet2.append([li_text])
 
 
 # All the text from page
@@ -61,5 +69,17 @@ for pa in gt:
     print(pa.get_text(strip=True))
     
 
-excel1.save('Images links.xlsx')
-excel2.save('Anchors links.xlsx')
+
+# To save the data using openpyxl
+#excel1.save('Images links.xlsx')
+#excel2.save('Anchors links.xlsx')
+
+
+
+
+# Use pandas only when all attributes have same length values
+fr = pd.DataFrame()
+fr['Image Links'] = im_list
+fr['Anchors Links'] = atag_list
+
+fr.to_csv(r'E:\Web Scraping\links.csv',index=True)
